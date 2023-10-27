f0f134f89ffa87ba73fc759743acb1008d442dda
Tyler Scotti <T.ScottiBusiness@yahoo.com>
2023-10-27 12:08:58 -0500

    Fix input func. Combine encode/decode funcs into one func. Expand range of valid shift amounts

--- 100DaysOfPythonBootcamp/Day8/Caesar Cipher/main.py
+++ 100DaysOfPythonBootcamp/Day8/Caesar Cipher/main.py
@@ -11,11 +11,11 @@ def getMessage(mode):
     return text
 
 def getMode():
-    mode = str(input("Type 'encode' to encypt, or 'decrypt' to decode: "))
+    mode = str(input("Type 'encode' to encypt, or 'decode' to decrypt: ")).lower()
 
     while(mode != "encode" and mode != "decode"):
         print("Invalid option.")
-        mode = str(input("Type 'encode' to encypt, or 'decrypt' to decode: "))
+        mode = str(input("Type 'encode' to encypt, or 'decrypt' to decode: ")).lower()
 
     if(mode == "encode"):
         return 1
@@ -23,16 +23,23 @@ def getMode():
         return 0
 
 def getShift():
-    shift = input("Enter in the shift amount: ")
+    shiftAmount = input("Enter in the shift amount: ")
 
-    while(not shift.isdigit()):
+    while(not shiftAmount.isdigit()):  
         print("Invalid option.")
-        shift = input("Enter in the shift amount: ")
+        shiftAmount = input("Enter in the shift amount: ")
 
-    return int(shift)
+    while(int(shiftAmount) <= 0):
+        print("Invalid option.")
+        shiftAmount = input("Enter in the shift amount: ")
+    return int(shiftAmount)
 
-def encodeMessage(messageStr, shiftAmount):
+def cipherFunc(messageStr, shiftAmount, mode):
     encodedMessage = ""
+    
+    #if it's to decode, turns shift amount to be negative
+    if(mode == 0):
+        shiftAmount *= -1
 
     for messageChar in messageStr:
         #non-alpha char check
@@ -46,39 +53,16 @@ def encodeMessage(messageStr, shiftAmount):
                 newCharIndex = index + shiftAmount
 
                 #handles index out of alphabet range
-                if(newCharIndex > 25):
+                if(newCharIndex > 25 or newCharIndex < -25):
                     newCharIndex %= 26
                 # print(f"New: {newCharIndex}") #DEBUG
                 encodedMessage += alphabet[newCharIndex]
     return encodedMessage
 
-def decodeMessage(encodedMessageStr, shiftAmount):
-    decodedMessage = ""
-
-    for messageChar in encodedMessageStr:
-        #non-alpha char check
-        if(not messageChar.isalpha()):
-            decodedMessage += messageChar
-
-        #I should really look up the built-in Py methods... didn't know alphabet.index(messageChar) would've been much easier
-
-        for (index, alphabetChar) in enumerate(alphabet):
-            # print(f"{index}: {alphabetChar}") #DEBUG
-            if(messageChar == alphabetChar):
-                newCharIndex = index - shiftAmount
-
-                # print(f"New: {newCharIndex}") #DEBUG
-                decodedMessage += alphabet[newCharIndex]
-    return decodedMessage
-
 def main(mode):
-    if(mode): #encode
-        newMessage = encodeMessage(getMessage(mode), getShift())
-        print(f"Encoded message: {newMessage}")
 
-    else: #deccode
-        newMessage = decodeMessage(getMessage(mode), getShift())
-        print(f"Decoded message: {newMessage}")
+    newMessage = cipherFunc(getMessage(mode), getShift(), mode)
+    print(f"Encoded message: {newMessage}")
 
 if __name__ == "__main__":
     main(getMode())
