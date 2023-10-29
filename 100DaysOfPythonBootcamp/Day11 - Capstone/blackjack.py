0faac1f84316f7ae4932d4c9d53f20ea5ad542a6
Tyler Scotti <T.ScottiBusiness@yahoo.com>
2023-10-28 20:49:10 -0500

    Finish player hand logic. Start dealer logic.

--- 100DaysOfPythonBootcamp/Day11 - Capstone/blackjack.py
+++ 100DaysOfPythonBootcamp/Day11 - Capstone/blackjack.py
@@ -22,33 +22,55 @@ cardValues = {
 def dealPlayerCard():
     card1 = card[random.randint(0, (len(card) - 1))]
     card2 = card[random.randint(0, (len(card) - 1))]
-
     return [card1, card2]
 
-def hit(hand):
+def dealDealerCard():
+    card1 = card[random.randint(0, (len(card) - 1))]
+    return [card1]
 
-    if(input("Type:\n'h': hit\n's': stand\n") == 's'):
-        return hand
-    
+def hit(playerHand):
+    while(True):
+        newCard = card[random.randint(0, (len(card) - 1))]
+        playerHand.append(newCard)
+
+        printHandAndTotal(playerHand, "player")
+
+        if(input("Type:\n'h': hit\n's': stand\n") == 's'):
+            return playerHand
+
+def printHandAndTotal(hand, person):
+    if(person.lower() == "player"):
+        print(f"Your cards are: {hand}")
+        print(f"Total: {calHandTotal(hand)}")
+    else:
+        print(f"Dealer Cards are: {hand}")
+        print(f"Total: {calHandTotal(hand)}")
+
+def calHandTotal(hand):
+    total = 0
+    for card in hand:
+        total += cardValues[str(card)]
+    return total
+
+def dealerHit(dealerHand):
     newCard = card[random.randint(0, (len(card) - 1))]
-    hand.append(cardValues[str(newCard)])
-    return hand
+    dealerHand.append(newCard)
+
+    while(calHandTotal(dealerHand) <= 16):
+        dealerHit(dealerHand)
+
+    return dealerHand
 
 def main():
-
     playerHand = dealPlayerCard()
-    print(f"Your cards are: {playerHand}")
-    print(f"Total: {cardValues[str(playerHand[0])] + cardValues[str(playerHand[1])]}")
+    printHandAndTotal(playerHand, "person")
 
-    playerHand = hit(playerHand)
+    if(input("Type:\n'h': hit\n's': stand\n") == 'h'):
+        hit(playerHand)
 
-    print(f"Your cards are: {playerHand}")
-
-    total = 0
-    for card in playerHand:
-        total += cardValues[str(card)]
-
-    print(f"Total: {total}")
+    dealerHand = dealDealerCard()
+    dealerHit(dealerHand)
+    printHandAndTotal(dealerHand, "dealer")
 
 if __name__ == "__main__":
     print(logo)
