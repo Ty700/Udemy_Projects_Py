be46f5c1edc5213124da83c4bb4d00e3f711db22
Tyler Scotti <T.ScottiBusiness@yahoo.com>
2023-11-04 22:54:14 -0500

    Finish Day20 and Day 21 - Snake Game

--- 100DaysOfPythonBootcamp/Day20_and_Day21/SnakeGame/main.py
+++ 100DaysOfPythonBootcamp/Day20_and_Day21/SnakeGame/main.py
@@ -1,19 +1,33 @@
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 from turtle import Screen
 import time
+
+X_BOUNDARY = 295
+Y_BOUNDARY = 295
 
 def configure_screen():
     screen = Screen()
     screen.setup(height=600,width=600)
+    screen
     screen.bgcolor("black")
     screen.tracer(0)
     screen.listen()
     return screen
 
+def snake_hit_wall(snake):
+        if(snake.head.xcor() > X_BOUNDARY or snake.head.xcor() < -X_BOUNDARY #x left or x Right
+           or snake.head.ycor() > Y_BOUNDARY or snake.head.ycor() < -Y_BOUNDARY): #y up or y down
+            return True
+        else:
+             return False   
+
 def main():
     screen = configure_screen()
-
     snake = Snake() 
+    food = Food()
+    score_board = ScoreBoard()
 
     screen.onkey(key="Up", fun=snake.up)
     screen.onkey(key="Left", fun=snake.left)
@@ -22,10 +36,30 @@ def main():
 
     game_is_on = True
     while(game_is_on): 
-
         screen.update()
         time.sleep(0.1)
         snake.move()
+
+        #snake has collided with the food
+        if(snake.head.distance(food) <= 15):
+            #move food
+            food.refresh_food_location()
+            #adds new segment
+            snake.extend()
+            #increase score 
+            score_board.increase_score()
+        
+        #detect collision with wall
+        if(snake_hit_wall(snake)):
+            game_is_on = False
+            score_board.game_over()
+
+        #Detect Collision with tail
+        #Skips head. snake_segments[0] = head
+        for segment in snake.snake_segments[1:]:
+            if(snake.head.distance(segment) < 10):
+                game_is_on = False
+                score_board.game_over()
 
     screen.exitonclick()
 if __name__ == "__main__":
--- 100DaysOfPythonBootcamp/Day20_and_Day21/SnakeGame/snake.py
+++ 100DaysOfPythonBootcamp/Day20_and_Day21/SnakeGame/snake.py
@@ -11,15 +11,26 @@ RIGHT = 0
 class Snake:
     def __init__(self) -> None:
         self.snake_segments = []
-        self.crete_snake()
+        self.create_snake()
         self.head = self.snake_segments[0]
-    def crete_snake(self):
+        self.head.color("green")
+
+    #initial creation of the snake
+    def create_snake(self):
         for position in START_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.snake_segments.append(new_segment)
+            self.add_segement(position)
+    
+    #adds a new segment in the snake_segment list to later create
+    def add_segement(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.snake_segments.append(new_segment)
+
+    #adds a new portion of the snake to the end of the current body
+    def extend(self):
+        self.add_segement(self.snake_segments[-1].position())
 
     def move(self):
         for segment_index in range(len(self.snake_segments)-1, 0, -1):
